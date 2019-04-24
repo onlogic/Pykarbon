f9ee0a2ba9da03e1b0397d4266ffb6cc175a0a24
Jacob Caughfield <jacob.caughfield@logicsupply.com>
2019-04-24 12:51:04 -0400

    Support for CI/CD: 	+ Add support for tox and pytest 	+ Create temporary 'dummy' test 	+ Add gitlab-ci config file

---
+++ test/test_basetools.py
@@ -0,0 +1,9 @@
+''' Tests for the basic underlying tools -- discovering ports, etc. '''
+
+def example(line):
+    ''' Returns the input line '''
+    return line
+
+def test_example():
+    ''' Check behavior of example '''
+    assert example("Test Message") == "Test Message"
