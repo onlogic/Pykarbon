onlogic/Pykarbon

commit 26fb459f5f8529dc14ae9731e03ad6804a1c0aa7

Keep registry threads asleep until data arrives
Fixes EEF-70

When background monitoring is enabled, the Terminal registry thread
consumes all available CPU time. A similar problem in the CAN registry
thread was worked around in 30ea4cc.

In both registry threads, an Event is now used to signal when data
arrives or when the interface is closing and the thread should shut
down. Arguably, letting Queue block would be simpler, but requires an ad
hoc handshake to tell the registry threads to shut down.

pykarbon/terminal.py
@@ -18,6 +18,7 @@ Example:
 from time import sleep, time
 import re
 import threading
+import queue
 
 import pykarbon.hardware as pk
 
@@ -60,11 +61,13 @@ class Session():
     '''
     def __init__(self, timeout=.01, automon=True):
         '''Discovers hardware port name. '''
+        self.pre_data = queue.Queue()
+        self.data = queue.Queue()
+        self.isopen = False
+        self.data_event = threading.Event()
+
         self.interface = pk.Interface('terminal', timeout)
 
-        self.pre_data = []
-        self.data = []
-        self.isopen = False
 
         self.info = {
             'version':
@@ -149,10 +152,7 @@ class Session():
             self.data = self.pre_data
 
     def __enter__(self):
-        if not self.isopen:
-            self.interface.__enter__()
-            self.isopen = True
-
+        self.open()
         return self
 
     def open(self):
@@ -172,7 +172,7 @@ class Session():
             line: Data that will be pushed onto the queue
         '''
 
-        self.data.append(line)
+        self.data.put(line)
 
     def register(self, input_num, state, action, **kwargs):
         '''Automatically perform action upon receiving data_id
@@ -392,7 +392,8 @@ class Session():
             line = self.interface.cread()[0]
             dio_check = re.match(r'[0-1]{4} {0,1}[0-1]{4}', line)
             if dio_check:
-                self.pre_data.append(line[0:4] + ' ' + line[4:8])
+                self.pre_data.put_nowait(line[0:4] + ' ' + line[4:8])
+                self.data_event.set()
             elif line:
                 self.parse_line(line)
 
@@ -406,9 +407,6 @@ class Session():
         Returns:
             The 'thread' object of this background process
         '''
-
-        if not self.data:
-            self.data = []
 
         self.bgmon = threading.Thread(target=self.monitor)
         self.bgmon.start()
@@ -442,13 +440,14 @@ class Session():
         into the main data queue. Otherwise, just move the data.
         '''
         while self.isopen:
+            self.data_event.clear()
             try:
-                line = self.pre_data.pop()
+                line = self.pre_data.get_nowait()
                 if line:
                     self.pushdata(line)
                     self.check_action(line)
-            except IndexError:
-                continue
+            except queue.Empty:
+                self.data_event.wait()
 
         return 0
 
@@ -536,8 +535,8 @@ class Session():
             String of the data read from the port. Returns empty string if the queue is empty
         '''
         try:
-            out = self.data.pop(0)
-        except IndexError:
+            out = self.data.get_nowait(0)
+        except queue.Empty:
             out = ""
 
         return out
@@ -549,7 +548,13 @@ class Session():
         connection, background monitoring will need to be manually restarted with the 'bgmonitor'
         method.
         '''
+        if not self.isopen:
+            return
+
         self.isopen = False
+
+        # Wake up the registry service thread so it will exit
+        self.data_event.set()
 
         try:
             if self.bgmon.is_alive():
@@ -560,19 +565,10 @@ class Session():
         self.interface.release()
 
     def __exit__(self, etype, evalue, etraceback):
-        self.isopen = False
-
-        try:
-            if self.bgmon.is_alive():
-                sleep(.1)
-        except AttributeError:
-            sleep(.001)
-
-        self.interface.__exit__(etype, evalue, etraceback)
+        self.close()
 
     def __del__(self):
-        if self.isopen:
-            self.close()
+        self.close()
 
 
 class Reactions():

pykarbon/can.py
@@ -21,6 +21,7 @@ Example:
 from time import sleep, time
 import threading
 import re
+import queue
 
 import pykarbon.hardware as pk
 
@@ -95,15 +96,16 @@ class Session():
     '''
     def __init__(self, baudrate='autobaud', timeout=.01, automon=True, reaction_poll_delay=.01):
         '''Discovers hardware port name.'''
-        self.interface = pk.Interface('can', timeout)
-
         self.poll_delay = reaction_poll_delay
         self.baudrate = None
-        self.pre_data = []
-        self.data = []
+        self.pre_data = queue.Queue()
+        self.data = queue.Queue()
+        self.data_event = threading.Event()
         self.isopen = False
         self.bgmon = None
         self.registry = {}
+
+        self.interface = pk.Interface('can', timeout)
 
         if baudrate == 'autobaud':
             self.autobaud(None)
@@ -117,10 +119,7 @@ class Session():
             self.data = self.pre_data
 
     def __enter__(self):
-        if not self.isopen:
-            self.interface.__enter__()
-            self.isopen = True
-
+        self.open()
         return self
 
     def open(self):
@@ -140,7 +139,8 @@ class Session():
             line: Data that will be pushed onto the queue
         '''
 
-        self.data.append(line.strip('\n\r'))
+        self.data.put_nowait(line.strip('\n\r'))
+        self.data_event.set()
 
     def autobaud(self, baudrate: int) -> str:
         '''Autodetect the bus baudrate
@@ -291,7 +291,7 @@ class Session():
         if self.isopen:
             line = self.interface.cread()[0]
             if line:
-                self.pre_data.append(line)
+                self.pre_data.put(line)
 
         return line
 
@@ -303,9 +303,6 @@ class Session():
         Returns:
             The 'thread' object of this background process
         '''
-
-        if not self.data:
-            self.data = []
 
         self.bgmon = threading.Thread(target=self.monitor)
         self.bgmon.start()
@@ -342,16 +339,14 @@ class Session():
         into the main data queue. Otherwise, just move the data.
         '''
         while self.isopen:
-            # Allow CPU to have time
-            sleep(self.poll_delay)
-
+            self.data_event.clear()
             try:
-                line = self.pre_data.pop(0)
+                line = self.pre_data.get_nowait()
                 if line:
                     self.check_action(line)
                     self.pushdata(line)
-            except IndexError:
-                continue
+            except queue.Empty:
+                self.data_event.wait()
 
         return 0
 
@@ -421,8 +416,8 @@ class Session():
             String of the data read from the port. Returns empty string if the queue is empty
         '''
         try:
-            out = self.data.pop(0)
-        except IndexError:
+            out = self.data.get()
+        except queue.Empty:
             out = ""
 
         return out
@@ -434,7 +429,13 @@ class Session():
         connection, background monitoring will need to be manually restarted with the 'bgmonitor'
         method.
         '''
+        if not self.isopen:
+            return
+
         self.isopen = False
+
+        # Wake up the registry service thread so it will exit
+        self.data_event.set()
 
         try:
             if self.bgmon.isAlive():
@@ -445,19 +446,10 @@ class Session():
         self.interface.release()
 
     def __exit__(self, etype, evalue, etraceback):
-        self.isopen = False
-
-        try:
-            if self.bgmon.isAlive():
-                sleep(.1)
-        except AttributeError:
-            sleep(.001)
-
-        self.interface.__exit__(etype, evalue, etraceback)
+        self.close()
 
     def __del__(self):
-        if self.isopen:
-            self.close()
+        self.close()
 
 
 class Reactions():

commit 24a8414dda828d54287acd52f436f339b3cebc9e

Fix bug which rendered Session.popdata() unusable
When calling popdata() on any session object, the method would immediately crash with `TypeError: get_nowait() takes 1 positional argument but 2 were given`.

The bug fixed here was introduced in [26fb459f][1] were the type of attribute `self.data` was changed from a standard `list` to `queue.Queue` and the list operation `data.pop(0)` (_remove first element_) was incorrectly translated to the Queue operation `data.pop_nowait(0)` which does not support any parameters, as it can only remove the first element.

[1]: https://github.com/onlogic/Pykarbon/commit/26fb459f5f8529dc14ae9731e03ad6804a1c0aa7#diff-880e8a0e1137d7e2c79e7b9aaadceb0bee90853b98c889c1a6bf474a2473f628L539-R538

pykarbon/terminal.py
@@ -532,7 +532,7 @@ class Session():
             String of the data read from the port. Returns empty string if the queue is empty
         '''
         try:
-            out = self.data.get_nowait(0)
+            out = self.data.get_nowait()
             self.prev_line = out
         except queue.Empty:
             out = ""